39b68ce648d6f78b5be8dcd8ad6e9625501a34ca
Chunjie Lu <lvchunjie00@gmail.com>
2018-10-30 23:53:36 -0400

    birthday

--- password-retry.py
+++ password-retry.py
@@ -1,10 +1,14 @@
-password = 'a123456'
+password = '19961017'
 i = 3
 while i > 0:
-	pwd = input('请输入密码:')
+	pwd = input('请输入陆晓婧的生日:')
 	if pwd == password:
-		print('登陆成功!')
+		print('回答正确!')
 		break
 	else:
 		i = i - 1
-		print('密码错误!还剩余', i ,'次机会')
+		print('回答错误!')
+		if i > 0:
+			print('还剩余', i ,'次机会')
+		else:
+			print('陆晓婧即将到达战场!')
